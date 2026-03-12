ec29da45273646710b15744e20b902fc2753d291
nitin <nitinghimire.w@gmail.com>
2026-03-12 19:42:26 +0545

    Add skeleton for the homepage of the blog

---
+++ index.js
@@ -0,0 +1,16 @@
+import express from "express";
+
+var app = express();
+const port = 3000;
+
+app.use(express.urlencoded({ extended: true }));
+
+app.use(express.static("public"));
+
+app.get("/", (req, res) => {
+    res.render("index.ejs");
+})
+
+app.listen(port, () => {
+    console.log(`Running the server on port : ${port}`);
+})
